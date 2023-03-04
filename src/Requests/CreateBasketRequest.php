efe81e9d13d92a6e658f421cec4096675a5d7b2a
pankovalxndr <pankovalxndr@gmail.com>
2023-03-04 15:03:11 +0300

    add auto send attribute to create basket request

--- src/Requests/CreateBasketRequest.php
+++ src/Requests/CreateBasketRequest.php
@@ -19,6 +19,17 @@ class CreateBasketRequest extends AbstractRequest implements RequestInterface
     public const RESPONSE_CLASS = CreateBasketResponse::class;
 
     /**
+     * Можно указать "T", тогда заказ сразу отправится в забор, минуя корзину.
+     * Идентично добавлению заказа в корзину,
+     * а затем его отправки через метод sendbasket (необязательный атрибут)
+     *
+     * @JMS\XmlAttribute()
+     * @JMS\Type("string")
+     * @JMS\SerializedName("autosend")
+     */
+    private ?string $autoSend = null;
+
+    /**
      * @JMS\Type("array<DalliSDK\Models\Order>")
      * @JMS\XmlList(inline = true, entry = "order")
      * @var Order[]
@@ -50,6 +61,32 @@ class CreateBasketRequest extends AbstractRequest implements RequestInterface
     public function forgetOrders(): CreateBasketRequest
     {
         $this->orders = [];
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getAutoSend(): bool
+    {
+        return $this->autoSend === 'T';
+    }
+
+    /**
+     * @return CreateBasketRequest
+     */
+    public function setAutoSend(): CreateBasketRequest
+    {
+        $this->autoSend = "T";
+        return $this;
+    }
+
+    /**
+     * @return CreateBasketRequest
+     */
+    public function removeAutoSend(): CreateBasketRequest
+    {
+        $this->autoSend = null;
         return $this;
     }
 }
--- tests/Integration/CreateBasketTest.php
+++ tests/Integration/CreateBasketTest.php
@@ -64,6 +64,7 @@ class CreateBasketTest extends SerializerTestCase
         $request = new CreateBasketRequest();
         $this->assertSame(CreateBasketResponse::class, $request->getResponseClass());
 
+        $this->assertFalse($request->getAutoSend());
 
         $request->addOrder($order);
         $this->assertContainsOnlyInstancesOf(Order::class, $request->getOrders());
@@ -73,6 +74,68 @@ class CreateBasketTest extends SerializerTestCase
 
         $request->forgetOrders();
         $this->assertSame([], $request->getOrders());
+    }
+
+    public function testSuccessfulSerializationWithAutoSend()
+    {
+        $xml = FixturesLoader::load('Order/CreateBasketWithAutoSendRequest.xml');
+
+        $items = [];
+        $item = new Item();
+        $order = new Order();
+        $receiver = new Receiver();
+
+        $receiver->setAddress('ул. Мусы Джалиля, д.2 к1')
+            ->setTown('г. Москва')
+            ->setPerson('Константин Константинопольский')
+            ->setPhone('+7 000 000 00 00')
+            ->setDate(new DateTime('2022-12-23'))
+            ->setTimeMin('18:00')
+            ->setTimeMax('22:00');
+
+        $item->setQuantity(2)
+            ->setName('Моя тестовая товарная позиция')
+            ->setWeight(3.15)
+            ->setRetPrice(50.0)
+            ->setInshPrice(5.0)
+            ->setOriginCountry('RU')
+            ->setGtd('10702030')
+            ->setSuppCompany('Компания поставщик')
+            ->setSuppPhone('+7 000 000 00 00')
+            ->setSuppInn('3664069397')
+            ->setType(1);
+        $items[] = $item;
+
+        $order->setNumber('sdk-002')
+            ->setReceiver($receiver)
+            ->setService(1)
+            ->setWeight(3.15)
+            ->setQuantity(1)
+            ->setPayType(PayType::CASH)
+            ->setPrice(150.0) // стоимость товарных позиций + стоимость доставки
+            ->setPriced(50.0)
+            ->setInshPrice(500.0)
+            ->setInstruction('Максимально аккуратно')
+            ->setItems($items);
+
+
+        $request = new CreateBasketRequest();
+        $request->setAutoSend();
+        $this->assertTrue($request->getAutoSend());
+        $this->assertSame(CreateBasketResponse::class, $request->getResponseClass());
+
+
+        $request->addOrder($order);
+        $this->assertContainsOnlyInstancesOf(Order::class, $request->getOrders());
+        $this->assertCount(1, $request->getOrders());
+
+        $this->assertSameXml($xml, $request);
+
+        $request->forgetOrders();
+        $this->assertSame([], $request->getOrders());
+
+        $request->removeAutoSend();
+        $this->assertFalse($request->getAutoSend());
     }
 
     public function testSuccessfulDeSerialization()
